597ed498272af01bdc3da3abe396f6c7bffff16e
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-24 15:25:02 +0200

    Add route

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ app.set("views", "views");
 app.set("template engine", "ejs");
 
 app.listen(3000, console.log("App is listening on http://localhost:3000/"));
+
+const ticketRouter = require("./routes/ticket");
+
+app.use("/support", ticketRouter);
