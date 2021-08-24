cdaeebb21fc2f5e5d104413e6a362854c7050ff7
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-24 15:02:10 +0200

    Set ejs as template engine

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.set("views", "views");
+app.set("template engine", "ejs");
+
 app.listen(3000, console.log("App is listening on http://localhost:3000/"));
