2502d560f28293e71288e4384f1c444f8835de52
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-25 12:51:21 +0200

    Add url encoded body parser

--- app.js
+++ app.js
@@ -10,4 +10,6 @@ app.listen(3000, console.log("App is listening on http://localhost:3000/"));
 
 const ticketRouter = require("./routes/ticket");
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/support", ticketRouter);
