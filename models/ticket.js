ea965f6b3823fd38c96f5391bd49b9fa045c3854
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-25 12:10:41 +0200

    Create ticket model with create function

---
+++ models/ticket.js
@@ -0,0 +1,39 @@
+//responsible for any operation performed on ticket tabel and its lookup tables
+
+const pool = require("../util/database");
+
+const Ticket = {};
+
+Ticket.create = async (options = {}) => {
+    //Inserts a new ticket using procedure
+    //Procedure parameters in order:
+    // ticket_id IS NOT NULL,
+    // customer_id IS NOT NULL,
+    // department_id IS NOT NULL,
+    // priority_level_id IS NOT NULL,
+    // channel_id IS NOT NULL,
+    // status_id DEFAULT NULL,
+    // category_id DEFAULT NULL,
+    // current_agent_id DEFAULT NULL
+
+    try {
+        //validate options param
+        if (options.attributes === {})
+            throw new Error("Options argrument was not passed any attributes");
+        const attributes = options.attributes;
+
+        return pool.execute("CALL insert_or_update_ticket(?, ?, ?, ?, ?, ?, ?)", [
+            attributes.customer_id || null,
+            attributes.department_id || null,
+            attributes.priority_level_id || null,
+            attributes.channel_id || null,
+            attributes.category_id || null,
+            attributes.status_id || null,
+            attributes.current_agent_id || null,
+        ]);
+    } catch (err) {
+        res.send(err);
+    }
+};
+
+module.exports = Ticket;
