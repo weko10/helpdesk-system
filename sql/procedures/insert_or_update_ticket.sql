61d6d7780d5cc63d3b936b44762db7969f67b3d7
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-25 11:06:54 +0200

    Create view and procedure for ticket model in engine

---
+++ sql/procedures/insert_or_update_ticket.sql
@@ -0,0 +1,36 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_or_update_ticket`(
+	_ticket_id INT,
+	_customer_id INT,
+    _department_id INT,
+    _status_id INT,
+    _priority_level_id INT,
+    _channel_id INT,
+    _category_id INT,
+    _current_agent_id INT
+)
+BEGIN
+	IF _ticket_id <= 0 THEN
+		INSERT INTO ticket (customer_id, department_id, category_id, status_id, priority_level_id, channel_id)
+        VALUES (_customer_id, _department_id, _category_id, _status_id, _priority_level_id, _channel_id);
+    ELSE
+		UPDATE ticket
+        SET
+			customer_id = ifnull(_customer_id, customer_id), 
+            department_id = ifnull(_department_id, department_id),
+            category_id = ifnull(_category_id, category_id),
+            current_agent_id = ifnull(_current_agent_id, current_agent_id),
+            status_id = ifnull(_status_id, status_id), 
+            priority_level_id = ifnull(_priority_level_id, priority_level_id), 
+            channel_id = ifnull(_channel_id, channel_id)
+		WHERE
+			id = _ticket_id AND (
+				_customer_id IS NOT NULL OR
+				_department_id IS NOT NULL OR
+				_category_id IS NOT NULL OR
+				_current_agent_id IS NOT NULL OR
+				_status_id IS NOT NULL OR
+				_priority_level_id IS NOT NULL OR
+				_channel_id IS NOT NULL
+            );
+		END IF;
+END
---
+++ sql/views/ticket_details.sql
@@ -0,0 +1,24 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `ticket_details` AS
+    SELECT 
+        `t`.`id` AS `ticket_id`,
+        `c`.`username` AS `customer_username`,
+        `d`.`name` AS `department`,
+        `ic`.`name` AS `issue_category`,
+        `a`.`username` AS `agent_username`,
+        `s`.`name` AS `status`,
+        `pl`.`name` AS `priority_level`,
+        `ch`.`name` AS `channel`,
+        `t`.`date_opened` AS `date_opened`
+    FROM
+        (((((((`ticket` `t`
+        JOIN `customer` `c` ON ((`t`.`customer_id` = `c`.`id`)))
+        JOIN `department` `d` ON ((`t`.`department_id` = `d`.`id`)))
+        LEFT JOIN `issue_category` `ic` ON ((`t`.`category_id` = `ic`.`id`)))
+        LEFT JOIN `agent` `a` ON ((`t`.`current_agent_id` = `a`.`id`)))
+        JOIN `status` `s` ON ((`t`.`status_id` = `s`.`id`)))
+        JOIN `priority_level` `pl` ON ((`t`.`priority_level_id` = `pl`.`id`)))
+        JOIN `channel` `ch` ON ((`t`.`channel_id` = `ch`.`id`)))
