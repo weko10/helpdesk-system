2c85120725f5d30c107b61f93bef00158969ddc0
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-25 12:06:03 +0200

    Modufy view

--- sql/views/ticket_details.sql
+++ sql/views/ticket_details.sql
@@ -12,13 +12,14 @@ VIEW `ticket_details` AS
         `s`.`name` AS `status`,
         `pl`.`name` AS `priority_level`,
         `ch`.`name` AS `channel`,
-        `t`.`date_opened` AS `date_opened`
+        `t`.`created_at` AS `created_at`
     FROM
         (((((((`ticket` `t`
         JOIN `customer` `c` ON ((`t`.`customer_id` = `c`.`id`)))
         JOIN `department` `d` ON ((`t`.`department_id` = `d`.`id`)))
         LEFT JOIN `issue_category` `ic` ON ((`t`.`category_id` = `ic`.`id`)))
         LEFT JOIN `agent` `a` ON ((`t`.`current_agent_id` = `a`.`id`)))
-        JOIN `status` `s` ON ((`t`.`status_id` = `s`.`id`)))
+        LEFT JOIN `status` `s` ON ((`t`.`status_id` = `s`.`id`)))
         JOIN `priority_level` `pl` ON ((`t`.`priority_level_id` = `pl`.`id`)))
         JOIN `channel` `ch` ON ((`t`.`channel_id` = `ch`.`id`)))
+    ORDER BY `t`.`created_at` DESC
