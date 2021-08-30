3449036aa2e0575840b314476d3f51e1ca0c8632
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-30 12:00:03 +0200

    Add routes for new views

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,15 @@
+exports.getSignup = (req, res) => {
+    res.render("auth/signup.ejs", {
+        pageTitle: "Sign Up",
+    });
+};
+
+exports.getLogin = (req, res) => {
+    res.render("auth/login.ejs", {
+        pageTitle: "Login In",
+    });
+};
+
 exports.getDashboardAccount = (req, res) => {
     //render dashboard with account info page
     res.render("auth/dashboard-account.ejs", {
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 
 const controller = require("../controllers/auth");
 
+router.get("/auth/signup", controller.getSignup);
+
+router.get("/auth/login", controller.getLogin);
+
 router.get("/dashboard/account", controller.getDashboardAccount);
 
 module.exports = router;
