16b5ecd76adfd75a403a30c350ad5aa357368ff5
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-09-01 01:44:25 +0200

    Quikc fix

--- controllers/support.js
+++ controllers/support.js
@@ -20,29 +20,31 @@ exports.getNewTicketForm = (req, res) => {
 
 exports.postNewTicket = async (req, res) => {
     try {
+        const { department_id, priority_level_id, subject, message } = req.body;
+
         //insert new ticket in database
         const [[[ticket]]] = await Ticket.create({
             attributes: {
-                customer_id: req.session.userData.id, //established on login
-                department_id: req.body.department_id,
-                priority_level_id: req.body.priority_level_id,
-                channel_id: 2,
-                status_id: 1,
+                customerId: req.session.userData.id, //established on login
+                departmentId: department_id,
+                priorityLevelId: priority_level_id,
+                channelId: 2,
+                statusId: 1,
             },
         });
 
         // insert initial message in database
         await Ticket.createMessage({
             attributes: {
-                ticket_id: ticket.ticket_id,
-                subject: req.body.subject,
-                body: req.body.message,
-                from_agent: 0,
+                ticketId: ticket.ticket_id,
+                subject: subject,
+                body: message,
+                fromAgent: 0,
             },
         });
 
         res.send({ message: "success" });
     } catch (err) {
-        res.send(err);
+        res.status(400).send(err);
     }
 };
