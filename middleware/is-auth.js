3a208f059492c182bb5c351d332eb467a4249059
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-09-02 14:01:37 +0200

    Add flash error

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,6 +2,6 @@ exports.isAuth = (req, res, next) => {
     if (req.session.isAuth) {
         return next();
     }
-    req.flash("message", "Please login with your account first");
+    req.flash("error", "Please login with your account first");
     return res.redirect(303, "/auth/login");
 };
