76116f09dfec0b23cefac2270f89d65ac28e8aa6
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-09-02 13:34:23 +0200

    Improve error handling

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const pool = require("../util/database");
 
 const User = {};
 
-User.create = (options = {}) => {
+User.create = async (options = {}) => {
     // _id INT,
     // _username varchar(64),
     // _email varchar(64),
@@ -16,65 +16,78 @@ User.create = (options = {}) => {
         //validate options param
         const attributes = options.attributes;
         if (attributes === undefined)
-            throw new Error("Options argrument was not passed any attributes");
+            throw Error("Options argrument was not passed any attributes");
 
-        return pool.execute("CALL insert_or_update_user(0, ?, ?, ?, ?, ?);", [
-            attributes.username || null,
-            attributes.email || null,
-            attributes.password || null,
-            attributes.phone || null,
-            attributes.homeAddress || null,
-        ]);
+        const result = await pool.execute(
+            "CALL insert_or_update_user(0, ?, ?, ?, ?, ?);",
+            [
+                attributes.username || null,
+                attributes.email || null,
+                attributes.password || null,
+                attributes.phone || null,
+                attributes.homeAddress || null,
+            ]
+        );
+
+        return result;
     } catch (err) {
-        throw new Error(err);
+        throw Error(err);
     }
 };
 
-User.findByPK = (options = {}) => {
+User.findByPK = async (options = {}) => {
     try {
         //validate options param
         const where = options.where;
         if (where === undefined)
-            throw new Error("Options argrument was not passed any where filters");
+            throw Error("Options argrument was not passed any where filters");
 
-        return pool.execute("SELECT * FROM user WHERE id = ?", [where.id || null]);
+        const result = await pool.execute("SELECT * FROM user WHERE id = ?", [
+            where.id || null,
+        ]);
+
+        return result;
     } catch (err) {
-        throw new Error(err);
+        throw Error(err);
     }
 };
 
-User.findAll = (options = {}) => {
+User.findAll = async (options = {}) => {
     // _id INT,
     // _email VARCHAR(64)
     try {
         //validate options param
         const where = options.where;
         if (where === undefined)
-            throw new Error("Options argrument was not passed any where filters");
+            throw Error("Options argrument was not passed any where filters");
 
-        return pool.execute("CALL get_all_users(?, ?)", [
+        const result = await pool.execute("CALL get_all_users(?, ?)", [
             where.id || null,
             where.email || null,
         ]);
+
+        return result;
     } catch (err) {
-        throw new Error(err);
+        throw Error(err);
     }
 };
 
-User.isExist = (options = undefined) => {
+User.isExist = async (options = {}) => {
     //Checks if a user exists by user id or email or both
     try {
         //validate options param
         const where = options.where;
-        if (where === {})
-            throw new Error("Options argrument was not passed any where filters");
+        if (where === undefined)
+            throw Error("Options argrument was not passed any where filters");
 
-        return pool.execute("SELECT is_user_exist(?, ?) AS result", [
+        const result = await pool.execute("SELECT is_user_exist(?, ?) AS result", [
             where.id || null,
             where.email || null,
         ]);
+
+        return result;
     } catch (err) {
-        throw new Error(err);
+        throw Error(err);
     }
 };
 
