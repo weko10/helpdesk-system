03014a0df264c78f9eb5712f43a67e3c431e13e4
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-09-07 01:52:37 +0200

    Fix custom error handlers

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.listen(3000, console.log("App is listening on http://localhost:3000/"));
 const supportRouter = require("./routes/support");
 const homeRouter = require("./routes/shop");
 const authRouter = require("./routes/auth");
-const error = require("./routes/error");
+const { notFound, logger, responder } = require("./controllers/error");
 
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: false }));
@@ -35,4 +35,15 @@ app.use(flush());
 app.use("/", homeRouter);
 app.use(authRouter);
 app.use(supportRouter);
-app.use(error);
+
+app.get("/throw", (req, res, next) => {
+    try {
+        const error = new Error("Async Broken");
+        error.statusCode = 500;
+        throw error;
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.use([notFound, logger, responder]);
--- controllers/error.js
+++ controllers/error.js
@@ -1,18 +1,16 @@
-//route not found
 exports.notFound = (req, res, next) => {
     const error = new Error("Page Not Found");
     error.statusCode = 404;
     next(error);
 };
 
-//logger
-exports.errorLogger = (err, req, res, next) => {
+exports.logger = (err, req, res, next) => {
     console.log(err);
     next(err);
 };
 
 //responder
-exports.errorResponder = (err, req, res, next) => {
+exports.responder = (err, req, res, next) => {
     if (err.statusCode >= 400 && err.statusCode < 500) {
         res.status(err.statusCode).render("error/4xx.ejs", {
             pageTitle: "Error",
