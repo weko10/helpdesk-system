2f2aae489ee33c1d7ad9c15ca4f042778ca56731
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-09-09 19:11:04 +0200

    Add ajax calls for ticket messages

--- public/js/ticket-chat.js
+++ public/js/ticket-chat.js
@@ -1,3 +1,8 @@
+const flash = document.getElementsByClassName("flash");
+
+const pagePath = window.location.pathname;
+const ticketId = pagePath.slice(18); //warning: bad practice
+
 //replace all iso dates with formated ones
 const dates = document.getElementsByClassName("date");
 for (let i = 0; i < dates.length; i++) {
@@ -5,3 +10,29 @@ for (let i = 0; i < dates.length; i++) {
     console.log(date.innerText);
     date.innerText = moment(date.innerText).format("DD.MM.yyyy (hh:mm)");
 }
+
+//fetch all ticket messages from api
+const fetchMessages = () => {
+    let result;
+
+    const xhr = new XMLHttpRequest();
+    const url = `http://localhost:3000/api/ticket/${ticketId}/messages`;
+    xhr.open("GET", url, true);
+
+    xhr.onload = () => {
+        result = JSON.parse(xhr.responseText);
+    };
+
+    xhr.onerror = () => {
+        flash.innerText = xhr.response;
+    };
+
+    xhr.send();
+
+    return result;
+};
+
+console.log(fetchMessages());
+
+//append ticket messages
+const appendMessages = () => {};
