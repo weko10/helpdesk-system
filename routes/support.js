09a2432c43fec7ed97187d3bd0a047d267924491
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-09-12 19:39:43 +0200

    Add validation logic

--- routes/support.js
+++ routes/support.js
@@ -1,13 +1,39 @@
 const router = require("express").Router();
 
 const controller = require("../controllers/support");
+const { check } = require("express-validator");
 const auth = require("../middleware/is-auth");
 
 router.get("/support", controller.getHome);
 
 router.get("/dashboard/new-ticket", auth.isAuth, controller.getNewTicketForm);
 
-router.post("/dashboard/new-ticket", auth.isAuth, controller.postNewTicket);
+router.post(
+    "/dashboard/new-ticket",
+    auth.isAuth,
+
+    //validation logic
+    [
+        check(
+            "department_id",
+            "Something is wrong with the chosen department. Please try again!"
+        ).isIn(["1", "2", "3"]),
+        check(
+            "priority_level_id",
+            "Something is wrong with the chosen priority. Please try again!"
+        ).isIn(["1", "2", "3"]),
+        check(
+            "subject",
+            "Something is wrong with the subject. Please try again!"
+        ).isByteLength({ max: 255 }),
+        check(
+            "message",
+            "Something is wrong with the message. Please try again!"
+        ).isByteLength({ min: 20, max: 1000 }),
+    ],
+
+    controller.postNewTicket
+);
 
 router.get("/dashboard/tickets", auth.isAuth, controller.getTicketsTable);
 
