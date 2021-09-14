e64348fc10fd814b98a79544fc7cb65625ea1bc9
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-09-15 01:48:12 +0200

    Quick fix

--- util/database.js
+++ util/database.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     user: process.env.MYSQLUSER,
     database: process.env.MYSQLDEFAULTDATABASE,
     password: process.env.MYSQLPASSWORD,
